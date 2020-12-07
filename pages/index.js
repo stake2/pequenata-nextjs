a106dfb444fb9f6dad3776c39ce623380aa0972a
stake2 <kiko12345kiko@hotmail.com>
2020-12-07 12:57:56 -0200

    Mudando algumas coisas

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,8 @@ import { useState } from 'react';
 function Home() {
     return (
         <div>
-            <h4>Home 5</h4>
-            <Contador />
+            <h1>Home 5</h1>
         </div>
     )
 }
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
-
 export default Home 
